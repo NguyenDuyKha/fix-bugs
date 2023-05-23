7b3aa655d957b10b6a45ee272e84c9854ed5a5e3
NguyenDuyKha <58772589+NguyenDuyKha@users.noreply.github.com>
2023-05-23 14:06:39 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ app.js
@@ -0,0 +1,29 @@
+const express = require('express')
+const cors = require('cors')
+const bodyParser = require('body-parser')
+const cookieParser = require('cookie-parser')
+const productRouter = require('./routes/productRoutes')
+const userRouter = require('./routes/userRoutes')
+const cartRouter = require('./routes/cartRoutes')
+const bookingRouter = require('./routes/bookingRoutes')
+
+const app = express()
+
+app.use(cors())
+app.use(express.static('./public/image'));
+
+app.use(cookieParser())
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+app.get('/' , (req,res) => {
+    res.status(200).json({
+        status : 'success',
+        message : "hello"
+    })
+})
+app.use('/myway/api/products', productRouter)
+app.use('/myway/api/users' , userRouter)
+
+app.use('/myway/api/carts' , cartRouter)
+app.use('/myway/api/bookings' , bookingRouter)
+module.exports = app
